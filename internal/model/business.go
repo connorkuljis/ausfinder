8007b0c4b3ad9ba691724001bb43b510f2cea49c
connorkuljis <conkuljis@gmail.com>
2025-01-07 12:34:03 +0800

    add model type: business

---
+++ internal/model/business.go
@@ -0,0 +1,13 @@
+package model
+
+type Business struct {
+	RegisterName string `db:"REGISTER_NAME"`
+	Name         string `db:"BN_NAME"`
+	Status       string `db:"BN_STATUS"`
+	RegisterDate string `db:"BN_REG_DT"` // eg: 18/10/2024
+	CancelDate   string `db:"BN_CANCEL_DT"`
+	RenewDate    string `db:"BN_RENEW_DT"`
+	StateNumber  string `db:"BN_STATE_NUM"`
+	StateOfReg   string `db:"BN_STATE_OF_REG"`
+	ABN          string `db:"BN_ABN"`
+}
