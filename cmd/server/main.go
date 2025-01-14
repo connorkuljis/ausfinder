ee9d282734c42298067f6657c1e50a640fda6da2
connorkuljis <conkuljis@gmail.com>
2025-01-14 22:12:04 +0800

    api: GET /company/:id

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,6 +30,7 @@ func main() {
 
 	e.GET("/", handleIndex(db))
 	e.GET("/search", handleSearch(db))
+	e.GET("/company/:id", handleCompany(db))
 
 	e.Start(":8080")
 }
@@ -42,7 +43,7 @@ func handleIndex(db *sqlx.DB) echo.HandlerFunc {
 	}
 
 	return func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+		return c.Render(http.StatusOK, "_index.html", map[string]interface{}{
 			"Count": count,
 		})
 	}
@@ -56,10 +57,10 @@ func handleSearch(db *sqlx.DB) echo.HandlerFunc {
 
 		var results []model.BusinessSearch
 		if queryStr != "" {
-			queryStr = queryStr + "*"
 			err := db.Select(&results, `SELECT * FROM business_search WHERE name MATCH ? AND state = ? ORDER BY abn DESC`, queryStr, stateStr)
 			if err != nil {
-				return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+				// TODO: don't throw error, but return error message to user.
+				// 	return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 			}
 		}
 
@@ -67,6 +68,21 @@ func handleSearch(db *sqlx.DB) echo.HandlerFunc {
 			"QueryStr": queryStr,
 			"Count":    len(results),
 			"Results":  results,
+		})
+	}
+}
+func handleCompany(db *sqlx.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id := c.Param("id")
+
+		var business model.Business
+		err := db.Get(&business, `SELECT * FROM business_names WHERE BN_ABN = ?`, id)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.Render(http.StatusOK, "_company-details.html", map[string]interface{}{
+			"Business": business,
 		})
 	}
 }
